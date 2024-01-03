154040481b24eef09f2529ca675e16f635b9665e
nayoung3669 <skdud3669@gmail.com>
2024-01-03 10:48:43 +0900

    :sparkles: [feat] : button component

--- src/app/components/common/Button.tsx
+++ src/app/components/common/Button.tsx
@@ -1,5 +1,53 @@
-const Button = () => {
-  return <div>Button</div>;
+'use client';
+
+interface ButtonProps {
+  buttonText: string;
+  buttonType: 'small' | 'medium' | 'large';
+  className: string;
+  isDisabled: boolean;
+  onClickHandler: () => void;
+}
+
+const getButtonStyles = (buttonType: 'small' | 'medium' | 'large') => {
+  switch (buttonType) {
+    case 'small':
+      return {
+        button: `px-2 py-2`,
+      };
+    case 'medium':
+      return {
+        button: `px-10 py-10`,
+      };
+    case 'large':
+      return {
+        button: `px-20 py-20`,
+      };
+    default:
+      return {
+        button: 'w-full h-full',
+      };
+  }
+};
+
+const Button = ({
+  onClickHandler,
+  buttonText,
+  buttonType,
+  className,
+  isDisabled,
+}: ButtonProps) => {
+  const buttonStyles = getButtonStyles(buttonType);
+
+  return (
+    <button
+      type="button"
+      className={`${buttonStyles.button} ${className}`}
+      onClick={onClickHandler}
+      disabled={isDisabled}
+    >
+      {buttonText}
+    </button>
+  );
 };
 
 export default Button;
