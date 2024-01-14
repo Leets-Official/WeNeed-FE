569ea37bca74206fbee317ab81256eedf67abc0a
One-HyeWon <5876675@naver.com>
2024-01-14 16:21:07 +0900

    :lipstick:[feat]: infobox ui 수정

--- src/app/components/mypage/profile/InfoBox.tsx
+++ src/app/components/mypage/profile/InfoBox.tsx
@@ -5,11 +5,11 @@ interface InfoBoxProps {
 
 const InfoBox = ({ title, content }: InfoBoxProps) => {
   return (
-    <div className="w-[356px] h-[50px] relative rounded-[10px] border border-neutral-400 flex-col justify-start items-start inline-flex">
-      <div className="w-[83.25px] text-white text-base font-semibold font-['Pretendard']">
+    <div className="w-[356px] h-[50px] relative rounded-[10px] border border-neutral-400 flex-row justify-between items-center inline-flex px-8">
+      <div className="text-white text-base font-semibold font-['Pretendard']">
         {title}
       </div>
-      <div className="w-[47.30px] text-right text-white text-base font-semibold font-['Pretendard']">
+      <div className="text-right text-white text-base font-semibold font-['Pretendard']">
         {content}
       </div>
     </div>
