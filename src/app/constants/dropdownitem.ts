e7bdff97318657e5ef9fb0224ab25b58108a6cd5
One-HyeWon <5876675@naver.com>
2024-01-15 15:03:14 +0900

    :lipstick:[feat]: tailwind css 수정

--- src/app/constants/dropdownitem.ts
+++ src/app/constants/dropdownitem.ts
@@ -3,8 +3,8 @@ import { MY_PAGE } from './mypage';
 const doubleMajorList = ['-', ...MY_PAGE.DEPARTMENT_LIST].sort();
 
 export const DROPDOWN_ITEM = {
-  grade: [...MY_PAGE.GRADE_LIST].sort(),
+  grade: [...MY_PAGE.GRADE_LIST],
   departmnet: [...MY_PAGE.DEPARTMENT_LIST].sort(),
-  interest: [...MY_PAGE.INTEREST_FIELD_LIST].sort(),
+  interest: [...MY_PAGE.INTEREST_FIELD_LIST],
   doublemajor: doubleMajorList,
 } as const;
