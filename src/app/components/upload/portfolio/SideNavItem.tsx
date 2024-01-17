877708a892e7144009347427db98eb92e18b4b4d
Minkyu0424 <yng1404@gmail.com>
2024-01-18 00:01:07 +0900

    :sparkles:[feat]: 사이드 바 UI

---
+++ src/app/components/upload/portfolio/SideNavItem.tsx
@@ -0,0 +1,20 @@
+import Icons from 'components/common/Icons';
+import { IconPathTypes } from '_types/icon';
+
+interface SideNavItemProps {
+  iconInfo: IconPathTypes;
+  label: string;
+}
+
+const SideNavItem = ({ iconInfo, label }: SideNavItemProps) => (
+  <div className="flex items-center flex-col gap-y-[10px]">
+    <div className="flex justify-center items-center w-[76px] h-[76px] bg-gradient-to-r from-cyan-400 to-blue-500 rounded-[56px]">
+      <Icons name={iconInfo} />
+    </div>
+    <div className="flex justify-center w-auto h-[21px] text-zinc-300 text-[16px]">
+      {label}
+    </div>
+  </div>
+);
+
+export default SideNavItem;
