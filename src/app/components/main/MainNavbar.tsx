9dd2c5bae7420eff5016fc0228564429eebafdd1
nayoung3669 <skdud3669@gmail.com>
2024-01-18 22:20:14 +0900

    :sparkles: [feat] : 카테고리 컴포넌트

--- src/app/components/main/MainNavbar.tsx
+++ src/app/components/main/MainNavbar.tsx
@@ -1,11 +1,26 @@
+'use client';
+
 import Icons from 'components/common/Icons';
-import { write } from 'ui/IconsPath';
+import { hamburger, write } from 'ui/IconsPath';
+import CategoriesBoxContainer from './containers/CategoriesBoxContainer';
+import { useState } from 'react';
 
 const MainNavbar = () => {
+  const [openCategoriesBox, setOpenCategoriesBox] = useState<boolean>(false);
+
   return (
-    <div className="w-full max-w-[1162px]">
+    <div className="relative w-full max-w-[1162px]">
       <div className="flex justify-between border-b border-white pb-[20px] mt-[50px] text-lg font-semibold">
         <ul className="flex gap-[50px] ">
+          <li
+            className="cursor-pointer flex items-center justify-center gap-3"
+            onClick={() => setOpenCategoriesBox(!openCategoriesBox)}
+          >
+            <p className="pb-1">
+              <Icons name={hamburger} />
+            </p>
+            카테고리
+          </li>
           <li className="cursor-pointer">게시물</li>
           <li className="cursor-pointer">크루찾기</li>
         </ul>
@@ -16,6 +31,7 @@ const MainNavbar = () => {
           <p>글쓰기</p>
         </div>
       </div>
+      {openCategoriesBox && <CategoriesBoxContainer />}
     </div>
   );
 };
