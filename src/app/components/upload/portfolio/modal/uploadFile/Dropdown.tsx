5e0d3f56859f3477238ce7184166c6895d0e8a17
Minkyu0424 <yng1404@gmail.com>
2024-01-19 10:41:55 +0900

    :sparkles:[feat] 각 파일,텍스트 추가 및 세부정보 입력창 UI

---
+++ src/app/components/upload/portfolio/modal/uploadFile/Dropdown.tsx
@@ -0,0 +1,67 @@
+import React, { useState } from 'react';
+import Icons from 'components/common/Icons';
+import { toggleIcon } from 'ui/IconsPath';
+
+interface DropdownProps {
+  options: string[];
+  title: string;
+  announcement: string;
+}
+
+const Dropdown: React.FC<DropdownProps> = ({
+  options,
+  title,
+  announcement,
+}: DropdownProps) => {
+  const [selectedOption, setSelectedOption] = useState<string>('');
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleOptionClick = (option: string) => {
+    setSelectedOption(option);
+    setIsOpen(false);
+  };
+
+  return (
+    <div className="flex flex-col">
+      <div
+        className="flex w-auto h-[49.96px] pl-[30px] pr-[27px] rounded-[10px] border border-zinc-300 items-center place-content-between cursor-pointer"
+        onClick={() => setIsOpen(!isOpen)}
+        onBlur={() => setIsOpen(false)}
+        tabIndex={0}
+      >
+        <div className="flex gap-x-[10px]">
+          <p>{title}</p>
+          <p className="text-red-400">*</p>
+        </div>
+        <div className="flex items-center">
+          {selectedOption ? (
+            <div className="text-black mr-[21px]">{selectedOption}</div>
+          ) : (
+            <div className="text-zinc-300 mr-[21px] ">{announcement}</div>
+          )}
+          <Icons name={toggleIcon} />
+        </div>
+      </div>
+      <div className="relative">
+        {isOpen && (
+          <div className="absolute top-full left-0 w-full h-[192px] py-[3px] bg-[#D9D9D9] rounded-[9px] overflow-y-auto">
+            {options.map((option, index) => (
+              <div
+                key={option}
+                className={`flex items-center w-full h-[37px] pl-[30px] py-[10px] cursor-pointer hover:bg-gray-100 border-t border-white ${
+                  index === 0 ? 'border-t-0' : ''
+                }
+                `}
+                onClick={() => handleOptionClick}
+              >
+                {option}
+              </div>
+            ))}
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Dropdown;
