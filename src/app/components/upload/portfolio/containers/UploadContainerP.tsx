75642d8d90f381e50cb879744c1fddb25515fd59
Minkyu0424 <yng1404@gmail.com>
2024-01-24 22:02:23 +0900

    :sparkles:[feat]: editorJS파일 생성및 작성UI에 추가

--- src/app/components/upload/portfolio/containers/UploadContainerP.tsx
+++ src/app/components/upload/portfolio/containers/UploadContainerP.tsx
@@ -1,13 +1,14 @@
 import { portThumbnail } from 'ui/bothUI';
 import AddThumbnail from '../../both/AddThumbnail';
 import SelectFile from '../SelectFileP';
+import Editor from 'components/upload/both/editor/Editor';
 
 const UploadContainerP = () => {
   return (
     <div className="flex flex-col w-full h-[1100px] bg-white">
       <AddThumbnail thumbnailInfo={portThumbnail} />
-      <div className="flex flex-col w-full h-[740px]">
-        <div>여긴 컨텐츠들 들어갈 곳</div>
+      <div className="flex flex-col w-full h-[740px] overflow-auto">
+        <Editor />
       </div>
       <div className="mb-[46px]">
         <SelectFile />
