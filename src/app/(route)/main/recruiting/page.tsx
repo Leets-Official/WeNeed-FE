b04c1add072d1d5edf86cc142f4184e61377b0b8
nayoung3669 <skdud3669@gmail.com>
2024-01-25 17:42:40 +0900

    :sparkles: [feat] : 리크루팅아이템 ui

--- src/app/(route)/main/recruiting/page.tsx
+++ src/app/(route)/main/recruiting/page.tsx
@@ -1,21 +1,17 @@
-import MainNavbar from 'components/main/common/MainNavbar';
 import { DetailCategoriesContainer } from 'components/main/containers';
 import RecruitingContainer from 'components/main/containers/RecruitingContainer';
 
 export default async function MainRecruitingPage() {
-  const data: ResponseRecruitingMain = await fetch(
+  const { recrutingList, pagable, user }: ResponseRecruitingMain = await fetch(
     `${process.env.NEXT_PUBLIC_NEXT_SERVER}/api/main/recruiting`,
-  )
-    .then((res) => res.json())
-    .then((res) => {
-      return res;
-    });
+    { cache: 'no-store' },
+  ).then((res) => res.json());
 
   return (
     <section className="flex flex-col items-center w-full min-h-screen text-white ">
-      <MainNavbar />
-      <DetailCategoriesContainer />
-      <RecruitingContainer />
+      {/* <MainNavbar /> */}
+      {/* <DetailCategoriesContainer /> */}
+      <RecruitingContainer data={recrutingList} />
     </section>
   );
 }
