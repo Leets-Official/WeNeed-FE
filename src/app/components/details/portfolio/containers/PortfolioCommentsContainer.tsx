c8e3545b250ac982c5ce6db88051b7a7b2dde2d1
nayoung3669 <skdud3669@gmail.com>
2024-01-28 23:48:27 +0900

    :sparkles: [feat] : 대댓글 ui 수정 (width), 아이콘 hover

--- src/app/components/details/portfolio/containers/PortfolioCommentsContainer.tsx
+++ src/app/components/details/portfolio/containers/PortfolioCommentsContainer.tsx
@@ -2,9 +2,9 @@
 
 import Icons from 'components/common/Icons';
 import Input from 'components/common/Input';
-import { useRef, useState } from 'react';
-import { commentClose, commentOpen, inputDrop } from 'ui/IconsPath';
 import CommentItem from 'components/details/common/CommentItem';
+import { useState } from 'react';
+import { commentClose, commentOpen, inputDrop } from 'ui/IconsPath';
 
 interface PortfolioCommentsContainerProps {
   comments: CommentList[];
@@ -18,33 +18,35 @@ const PortfolioCommentsContainer = ({
 
   const onSubmitHandler = () => {};
   return (
-    <div className="flex flex-col items-center w-full bg-white pt-[50px] pb-[100px]">
-      <div className="min-w-[1290px]">
-        <p className="w-full text-xl font-semibold mb-[25px]">
-          댓글 {comments.length}개
-        </p>
-        <div className="flex justify-between items-center relative">
-          <div className="w-[56px] h-[56px] bg-neutral-300 rounded-full "></div>
-          <Input
-            type="comment"
-            textValue={commentValue}
-            onChange={(e) => setCommentValue(e.target.value)}
-            onEnterPress={onSubmitHandler}
-            placeholder="댓글을 입력해주세요."
-          />
-          <div className="absolute top-[15px] right-[19px] flex justify-center items-center w-6 h-6 rounded-full bg-gradient-to-r from-[#4EF4FF] to-[#608CFF] cursor-pointer">
-            <Icons name={inputDrop} />
-          </div>
+    <div className="flex flex-col justify-center items-center w-full bg-white pt-[50px] pb-[100px] ">
+      <p className="text-xl font-semibold mb-[25px] w-[1290px]">
+        댓글 {comments.length}개
+      </p>
+      <div className="w-[1290px] flex items-center relative">
+        <div className="w-[56px] h-[56px] bg-neutral-300 rounded-full mr-[23px]"></div>
+        <Input
+          type="comment"
+          textValue={commentValue}
+          onChange={(e) => setCommentValue(e.target.value)}
+          onEnterPress={onSubmitHandler}
+          placeholder="댓글을 입력해주세요."
+        />
+        <div className="absolute top-[15px] right-[19px] flex justify-center items-center w-6 h-6 rounded-full bg-gradient-to-r from-[#4EF4FF] to-[#608CFF] cursor-pointer">
+          <Icons name={inputDrop} />
         </div>
+      </div>
+      <div className="w-[1290px] flex flex-col items-start">
         {comments.length ? (
           comments.map((comment) => {
             const { children, commentId } = comment;
             return (
-              <div key={commentId} className="flex flex-col w-auto">
+              <div key={commentId}>
                 <CommentItem comment={comment} />
                 {comment.children && (
                   <div
-                    className="w-[98px] h-[20px] rounded-[10px] bg-gradient-to-r from-[#00E0EE] to-[#517EF3] text-[12px] text-center font-normal flex justify-center items-center ml-[75px] mt-[13px] gap-1"
+                    className="w-[98px] h-[20px] rounded-[10px] bg-gradient-to-r from-[#00E0EE] to-[#517EF3]
+                      text-[12px] text-center font-normal flex justify-center items-center ml-[75px] mt-[13px] gap-1 
+                      cursor-pointer hover:from-[#fff] hover:to-[#fff]"
                     onClick={() => setOpenChildren(!openChildren)}
                   >
                     {openChildren ? (
@@ -59,14 +61,16 @@ const PortfolioCommentsContainer = ({
                     <p>답글 {children?.length}개 보기</p>
                   </div>
                 )}
-                {openChildren &&
-                  children?.map((recomment) => {
-                    return (
-                      <div key={recomment.commentId} className="ml-[75px]">
-                        <CommentItem comment={recomment} />
-                      </div>
-                    );
-                  })}
+                <div className="flex flex-col items-start">
+                  {openChildren &&
+                    children?.map((recomment) => {
+                      return (
+                        <div key={recomment.commentId} className="ml-[75px]">
+                          <CommentItem comment={recomment} />
+                        </div>
+                      );
+                    })}
+                </div>
               </div>
             );
           })
