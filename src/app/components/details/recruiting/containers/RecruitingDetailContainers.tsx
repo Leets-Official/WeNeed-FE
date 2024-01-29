1dc8474dc7af0dbb95a649706be56b74a859ce41
nayoung3669 <skdud3669@gmail.com>
2024-01-29 13:35:12 +0900

    :sparkles: [feat] : 로그인 유도 모달 ui

--- src/app/components/details/recruiting/containers/RecruitingDetailContainers.tsx
+++ src/app/components/details/recruiting/containers/RecruitingDetailContainers.tsx
@@ -1,3 +1,10 @@
+'use client';
+
+import ModalOutside from 'components/common/modal/ModalOutside';
+import ModalPortal from 'components/common/modal/ModalPortal';
+import NeedLoginModal from 'components/common/modal/NeedLoginModal';
+import { useEffect, useState } from 'react';
+
 interface RecruitingDetailContainersProps {
   data: RecruitDetailItem;
 }
@@ -5,10 +12,34 @@ interface RecruitingDetailContainersProps {
 const RecruitingDetailContainers = ({
   data,
 }: RecruitingDetailContainersProps) => {
+  const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
+
+  useEffect(() => {
+    setIsLoggedIn(true);
+  }, []);
+
+  useEffect(() => {
+    if (isLoggedIn) {
+      document.body.style.overflow = 'hidden';
+    }
+  }, [isLoggedIn]);
+
   return (
-    <div className="w-[1280px] min-h-screen bg-white rounded-[10px] mt-[40px] px-[47px] py-[54px]">
-      RecruitingDetailContainers
-    </div>
+    <>
+      <div className="w-[1280px] min-h-screen bg-white rounded-[10px] mt-[40px] px-[47px] py-[54px]">
+        ㅚㄴ니니니
+      </div>
+      {isLoggedIn && (
+        <ModalPortal nodeName="needLoginPortal">
+          <ModalOutside
+            onClose={() => {}}
+            className="absolute top-[63px] left-0 w-full h-full flex justify-center items-center "
+          >
+            <NeedLoginModal />
+          </ModalOutside>
+        </ModalPortal>
+      )}
+    </>
   );
 };
 
---
+++ src/app/constants/common.ts
@@ -0,0 +1,4 @@
+export const NEED_LOGIN_MODAL = {
+  title: '를 로그인 하고 사용해주세요.',
+  sub_title: '로그인 후 업로드된 다양한 크루찾기를 열람할 수 있어요.',
+} as const;
