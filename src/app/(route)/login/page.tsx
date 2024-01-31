f82084c7d3976ecc91f6a8f3b203eae027105162
One-HyeWon <5876675@naver.com>
2024-02-01 02:37:33 +0900

    :sparkles:[feat]: home 아이콘 링크 추가

--- src/app/(route)/login/page.tsx
+++ src/app/(route)/login/page.tsx
@@ -1,17 +1,20 @@
 import Icons from 'components/common/Icons';
 import GoogleLoginButton from 'components/login/GoogleLoginButton';
+import Link from 'next/link';
 import { loginHome, loginLeest, loginWeneed } from 'ui/IconsPath';
 
 const LoginPage = () => {
   return (
-    <div className="bg-login w-screen h-screen bg-cover bg-no-repeat bg-fixed overflow-hidden flex justify-center">
+    <div className="bg-login w-screen h-screen bg-cover bg-no-repeat bg-fixed flex justify-center scrollbar-hide">
       <Icons name={loginLeest} className="absolute top-2" />
       <div className="absolute top-0 mt-44 w-[530px] h-[68px] text-white text-[64px] font-bold font-['Pretendard']">
         WE NEED, WIN IT.
       </div>
       <Icons name={loginWeneed} className="absolute top-0 mt-60" />
       <GoogleLoginButton />
-      <Icons name={loginHome} className="absolute bottom-5" />
+      <Link href="/main">
+        <Icons name={loginHome} className="absolute bottom-5" />
+      </Link>
     </div>
   );
 };
