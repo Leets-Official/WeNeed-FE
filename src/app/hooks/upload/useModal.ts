2753d90ed2005bcf011400766206b6959641c33c
Minkyu0424 <yng1404@gmail.com>
2024-02-02 05:45:26 +0900

    :sparkles:[feat]: useModal hook생성

---
+++ src/app/hooks/upload/useModal.ts
@@ -0,0 +1,27 @@
+'use client';
+import { useState } from 'react';
+
+export const useModal = (initialState = false) => {
+  const [isOpen, setIsOpen] = useState(initialState);
+
+  const openModal = () => {
+    setIsOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsOpen(false);
+  };
+
+  const handleModalClick = (
+    event: React.MouseEvent<HTMLDivElement, MouseEvent>,
+  ) => {
+    event.stopPropagation();
+  };
+
+  return {
+    isOpen,
+    openModal,
+    closeModal,
+    handleModalClick,
+  };
+};
