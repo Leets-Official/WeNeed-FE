8b1d9566919f6520e0395edf1365b6ca85b07665
One-HyeWon <5876675@naver.com>
2024-02-03 13:16:59 +0900

    :recycle:[refactor]: 텍스트 상수화 적용

--- src/app/(route)/login/page.tsx
+++ src/app/(route)/login/page.tsx
@@ -2,6 +2,7 @@ import Icons from 'components/common/Icons';
 import GoogleLoginButton from 'components/login/GoogleLoginButton';
 import Link from 'next/link';
 import { loginHome, loginLeest, loginWeneed } from 'ui/IconsPath';
+import { LOGIN_SUCCESS } from 'constants/userinfoset';
 
 const LoginPage = () => {
   return (
@@ -9,7 +10,7 @@ const LoginPage = () => {
       <div className="bg-login w-screen h-screen bg-cover bg-no-repeat bg-fixed flex justify-center">
         <Icons name={loginLeest} className="absolute top-2" />
         <div className="absolute top-0 mt-44 w-[530px] h-[68px] text-white text-[64px] font-bold font-['Pretendard']">
-          WE NEED, WIN IT.
+          {LOGIN_SUCCESS.TITLE}
         </div>
         <Icons name={loginWeneed} className="absolute top-0 mt-60" />
         <GoogleLoginButton />
