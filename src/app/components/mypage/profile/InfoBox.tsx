27270f896aeb9df2bf86724a1669b230b7d1c2ee
One-HyeWon <5876675@naver.com>
2024-02-04 21:41:43 +0900

    :sparkles:[feat]: dropdown 컴포넌트 수정

--- src/app/components/mypage/profile/InfoBox.tsx
+++ src/app/components/mypage/profile/InfoBox.tsx
@@ -5,13 +5,9 @@ interface InfoBoxProps {
 
 const InfoBox = ({ title, content }: InfoBoxProps) => {
   return (
-    <div className="w-[356px] h-[50px] relative rounded-[10px] border border-neutral-400 flex-row justify-between items-center inline-flex px-8">
-      <div className="text-white text-base font-semibold font-['Pretendard']">
-        {title}
-      </div>
-      <div className="text-right text-white text-base font-semibold font-['Pretendard']">
-        {content}
-      </div>
+    <div className="w-[356px] h-[50px] relative rounded-[10px] border border-neutral-400 flex-row justify-between items-center flex px-8">
+      <div className="text-white font-semibold">{title}</div>
+      <div className="text-right text-white font-semibold">{content}</div>
     </div>
   );
 };
