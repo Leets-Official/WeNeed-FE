4f7daf894094206a1dfd5bfb7f9d853b00808358
One-HyeWon <5876675@naver.com>
2024-02-05 04:09:19 +0900

    :recycle:[refactor]: resolved conflict

--- src/app/components/details/portfolio/containers/PortfolioCommentsContainer.tsx
+++ src/app/components/details/portfolio/containers/PortfolioCommentsContainer.tsx
@@ -2,9 +2,9 @@
 
 import Icons from 'components/common/Icons';
 import Input from 'components/common/Input';
-import Image from 'next/image';
 import { useRef, useState } from 'react';
-import { inputDrop } from 'ui/IconsPath';
+import { commentClose, commentOpen, inputDrop } from 'ui/IconsPath';
+import CommentItem from 'components/details/common/CommentItem';
 
 interface PortfolioCommentsContainerProps {
   comments: CommentList[];
@@ -14,19 +14,16 @@ const PortfolioCommentsContainer = ({
   comments = [],
 }: PortfolioCommentsContainerProps) => {
   const [commentValue, setCommentValue] = useState<string>('');
-  const commentsContainerRef = useRef(null);
+  const [openChildren, setOpenChildren] = useState<boolean>(false);
 
   const onSubmitHandler = () => {};
   return (
-    <div
-      className="flex flex-col justify-center items-center w-full bg-white py-[50px]"
-      ref={commentsContainerRef}
-    >
-      <div className="w-[1290px]">
+    <div className="flex flex-col items-center w-full bg-white pt-[50px] pb-[100px]">
+      <div className="min-w-[1290px]">
         <p className="w-full text-xl font-semibold mb-[25px]">
           댓글 {comments.length}개
         </p>
-        <div className="flex justify-center items-center relative gap-[20px]">
+        <div className="flex justify-between items-center relative">
           <div className="w-[56px] h-[56px] bg-neutral-300 rounded-full "></div>
           <Input
             type="comment"
@@ -35,13 +32,47 @@ const PortfolioCommentsContainer = ({
             onEnterPress={onSubmitHandler}
             placeholder="댓글을 입력해주세요."
           />
-          <div className="absolute top-[14px] right-[19px] flex justify-center items-center w-6 h-6 rounded-full bg-gradient-to-r from-[#4EF4FF] to-[#608CFF] cursor-pointer">
+          <div className="absolute top-[15px] right-[19px] flex justify-center items-center w-6 h-6 rounded-full bg-gradient-to-r from-[#4EF4FF] to-[#608CFF] cursor-pointer">
             <Icons name={inputDrop} />
           </div>
         </div>
-        {comments.map((comment) => {
-          return <p key={comment.commentId}>{comment.content}</p>;
-        })}
+        {comments.length ? (
+          comments.map((comment) => {
+            const { children, commentId } = comment;
+            return (
+              <div key={commentId} className="flex flex-col w-auto">
+                <CommentItem comment={comment} />
+                {comment.children && (
+                  <div
+                    className="w-[98px] h-[20px] rounded-[10px] bg-gradient-to-r from-[#00E0EE] to-[#517EF3] text-[12px] text-center font-normal flex justify-center items-center ml-[75px] mt-[13px] gap-1"
+                    onClick={() => setOpenChildren(!openChildren)}
+                  >
+                    {openChildren ? (
+                      <p className="py-1">
+                        <Icons name={commentClose} />
+                      </p>
+                    ) : (
+                      <p className="h-3">
+                        <Icons name={commentOpen} />
+                      </p>
+                    )}
+                    <p>답글 {children?.length}개 보기</p>
+                  </div>
+                )}
+                {openChildren &&
+                  children?.map((recomment) => {
+                    return (
+                      <div key={recomment.commentId} className="ml-[75px]">
+                        <CommentItem comment={recomment} />
+                      </div>
+                    );
+                  })}
+              </div>
+            );
+          })
+        ) : (
+          <div>가장 먼저 댓글을 작성해보세요!</div>
+        )}
       </div>
     </div>
   );
