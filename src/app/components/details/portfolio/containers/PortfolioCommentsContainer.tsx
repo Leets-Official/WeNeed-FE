a42cbacf378260b85ba5ffc5f38e96ad84f2ec51
One-HyeWon <5876675@naver.com>
2024-02-05 04:28:31 +0900

    :recycle:[refactor]: resolved conflict

--- src/app/components/details/portfolio/containers/PortfolioCommentsContainer.tsx
+++ src/app/components/details/portfolio/containers/PortfolioCommentsContainer.tsx
@@ -3,39 +3,68 @@
 import Icons from 'components/common/Icons';
 import Input from 'components/common/Input';
 import CommentItem from 'components/details/common/CommentItem';
-import { useState } from 'react';
+import { NO_COMMENTS } from 'constants/common';
+import { useEffect, useState } from 'react';
 import { commentClose, commentOpen, inputDrop } from 'ui/IconsPath';
 
-interface PortfolioCommentsContainerProps {
+interface CommentsContainerProps {
+  onRecruit?: boolean;
   comments: CommentList[];
 }
 
-const PortfolioCommentsContainer = ({
+const CommentsContainer = ({
+  onRecruit = false,
   comments = [],
-}: PortfolioCommentsContainerProps) => {
+}: CommentsContainerProps) => {
   const [commentValue, setCommentValue] = useState<string>('');
   const [openChildren, setOpenChildren] = useState<boolean>(false);
+  const [totalComments, setTotalComments] = useState<number>(0);
+
+  useEffect(() => {
+    const countComments = () => {
+      let cnt = comments.length;
+      comments.map((comment) => {
+        cnt += comment.children?.length || 0;
+      });
+      return cnt;
+    };
+    setTotalComments(countComments());
+  }, [comments]);
 
   const onSubmitHandler = () => {};
+
   return (
-    <div className="flex flex-col justify-center items-center w-full bg-white pt-[50px] pb-[100px] ">
-      <p className="text-xl font-semibold mb-[25px] w-[1290px]">
-        댓글 {comments.length}개
+    <div className="flex flex-col justify-center items-center w-full bg-white pt-[50px] pb-[100px]  ">
+      <p
+        className={`text-xl font-semibold mb-[25px] max-w-[1290px]  ${
+          onRecruit ? 'text-black w-full' : ' w-[79%] '
+        }`}
+      >
+        댓글{totalComments}개
       </p>
-      <div className="w-[1290px] flex items-center relative">
-        <div className="w-[56px] h-[56px] bg-neutral-300 rounded-full mr-[23px]"></div>
+      <div
+        className={`flex items-center relative max-w-[1290px] ${
+          onRecruit ? ' w-full' : ' w-[79%]'
+        }`}
+      >
+        <div className="w-[56px] h-[56px] bg-neutral-300 rounded-full mr-[23px] "></div>
         <Input
           type="comment"
           textValue={commentValue}
           onChange={(e) => setCommentValue(e.target.value)}
           onEnterPress={onSubmitHandler}
           placeholder="댓글을 입력해주세요."
+          className="w-[90%]"
         />
-        <div className="absolute top-[15px] right-[19px] flex justify-center items-center w-6 h-6 rounded-full bg-gradient-to-r from-[#4EF4FF] to-[#608CFF] cursor-pointer">
+        <div className="absolute top-[30%] right-[4.5%] flex justify-center items-center w-6 h-6 rounded-full bg-gradient-to-r from-[#4EF4FF] to-[#608CFF] cursor-pointer">
           <Icons name={inputDrop} />
         </div>
       </div>
-      <div className="w-[1290px] flex flex-col items-start">
+      <div
+        className={` mt-[30px] flex flex-col items-start text-black max-w-[1290px] ${
+          onRecruit ? ' w-full ' : ' w-[79%]'
+        }`}
+      >
         {comments.length ? (
           comments.map((comment) => {
             const { children, commentId } = comment;
@@ -45,7 +74,7 @@ const PortfolioCommentsContainer = ({
                 {comment.children && (
                   <div
                     className="w-[98px] h-[20px] rounded-[10px] bg-gradient-to-r from-[#00E0EE] to-[#517EF3]
-                      text-[12px] text-center font-normal flex justify-center items-center ml-[75px] mt-[13px] gap-1 
+                      text-[12px] text-center font-normal flex justify-center items-center ml-[86px] mt-[13px] gap-1 
                       cursor-pointer hover:from-[#fff] hover:to-[#fff]"
                     onClick={() => setOpenChildren(!openChildren)}
                   >
@@ -58,7 +87,7 @@ const PortfolioCommentsContainer = ({
                         <Icons name={commentOpen} />
                       </p>
                     )}
-                    <p>답글 {children?.length}개 보기</p>
+                    <p className="pt-0.5">답글 {children?.length}개 보기</p>
                   </div>
                 )}
                 <div className="flex flex-col items-start">
@@ -75,11 +104,11 @@ const PortfolioCommentsContainer = ({
             );
           })
         ) : (
-          <div>가장 먼저 댓글을 작성해보세요!</div>
+          <p>{NO_COMMENTS.title}</p>
         )}
       </div>
     </div>
   );
 };
 
-export default PortfolioCommentsContainer;
+export default CommentsContainer;
