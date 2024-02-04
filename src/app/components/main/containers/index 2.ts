03bf8d5452ad0562d726101bfab9356827d5535c
One-HyeWon <5876675@naver.com>
2024-02-04 22:51:14 +0900

    :recycle:[refactor]: 중복 파일 제거

--- src/app/components/main/containers/index 2.ts
+++
@@ -1,4 +0,0 @@
-export { default as DetailCategoriesContainer } from './DetailCategoriesContainer';
-export { default as PortfolioContainer } from './PortfolioContainer';
-export { default as HotPortfolioContainer } from './HotPortfolioContainer';
-export { default as RecommendContainer } from './RecommendContainer';
