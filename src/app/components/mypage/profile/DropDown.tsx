4937654fd495cec6d0ab9ca06be64354f2ead628
One-HyeWon <5876675@naver.com>
2024-02-04 22:28:56 +0900

    :sparkles:[feat]: DropDown 컴포넌트 설정

--- src/app/components/mypage/profile/DropDown.tsx
+++ src/app/components/mypage/profile/DropDown.tsx
@@ -1,4 +1,7 @@
 'use client';
+import Icons from 'components/common/Icons';
+import { MY_PAGE } from 'constants/mypage';
+import { userinfoSetDownAngle, userinfoSetUpAngle } from 'ui/IconsPath';
 
 interface DropDownProps {
   sortedItemList: string[];
@@ -19,34 +22,27 @@ const DropDown = ({
     <>
       <button
         type="button"
-        className={`w-[312px] h-[48px] rounded-[16px] bg-gray-500 flext justify-between items-center px-4`}
+        className={`w-[312px] h-[48px] rounded-[16px] bg-gray-500 relative flex justify-between items-center p-4`}
         onClick={onOpen}
       >
-        <span className="text-black ">{selectedItem}</span>
-        <span>
-          <svg
-            width="18"
-            height="9"
-            viewBox="0 0 18 9"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M1.28125 1.08231L9.28125 7.93945L17.2813 1.08231"
-              stroke="#8C8C8C" // mypage, loginpage에 따라 색상 다르게 주기
-              strokeMiterlimit="10"
-              strokeLinecap="square"
-            />
-          </svg>
+        <span className="text-black">{selectedItem || MY_PAGE.MAJOR}</span>
+        <span className="absolute right-4">
+          {isOpen ? (
+            <Icons name={userinfoSetUpAngle} />
+          ) : (
+            <Icons name={userinfoSetDownAngle} />
+          )}
         </span>
       </button>
 
       {isOpen && (
-        <div className="absolute bottom-[230px] left-0 w-[312px] max-h-48 rounded-[16px] overflow-y-scroll scrollbar-hide bg-onepink">
+        <div className="absolute bottom-[230px] left-0 w-[312px] max-h-48 rounded-[16px] overflow-y-scroll scrollbar-hide bg-white">
           {sortedItemList.map((item) => (
             <button
               key={item}
-              className={`w-full h-12 hobver:text-black `}
+              className={`w-full h-12 hover:text-black ${
+                selectedItem === item ? 'text-black' : 'text-gray-500'
+              }`}
               onClick={() => onItemSelect(item)}
               type="button"
             >
