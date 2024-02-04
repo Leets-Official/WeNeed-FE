3b096283513ee42d3ddfa608b21abe7a325b593c
One-HyeWon <5876675@naver.com>
2024-02-05 04:22:21 +0900

    :recycle:[refactor]: resolved conflict

---
+++ src/app/components/upload/portfolio/containers/FileTypeContainer.tsx
@@ -0,0 +1,19 @@
+import { FILE_TYPE_LIST } from 'constants/portfolio';
+import FileType from '../FileType';
+import { fileIcon, fileIconPath } from 'ui/IconsPath';
+
+const FileTypeContainer = () => {
+  return (
+    <div className="flex gap-x-[25px]">
+      {FILE_TYPE_LIST.map((item, index) => (
+        <FileType
+          key={item}
+          title={item}
+          iconSvgInfo={{ ...fileIcon, path: fileIconPath[index] }}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default FileTypeContainer;
