41a8f47b2606b553a30ce4dd04ff249d22d24b5d
One-HyeWon <5876675@naver.com>
2024-02-09 14:12:51 +0900

    :recycle:[refactor]: rebase

---
+++ src/app/components/upload/both/editor/EditorTools.js
@@ -0,0 +1,11 @@
+import Paragraph from '@editorjs/paragraph';
+import image from '@editorjs/simple-image';
+import LinkTool from '@editorjs/link';
+import AttachesTool from '@editorjs/attaches';
+
+export const EDITOR_TOOLS = {
+  paragraph: Paragraph,
+  image: image,
+  linkTool: LinkTool,
+  attaches: AttachesTool,
+};
