c6cfce5ae0c2debab13d8c71718ff3b4ebe6e116
Minkyu0424 <yng1404@gmail.com>
2024-02-09 20:43:02 +0900

    :sparkles:[feat]:포트폴리오 data(팀원 제외) recoil관리

--- src/app/_types/upload.ts
+++ src/app/_types/upload.ts
@@ -69,3 +69,18 @@ interface RecruitmentComplete {
   content: string;
   keywords: string;
 }
+
+interface ContentTypes {
+  id: string;
+  type: string;
+  data: string;
+}
+
+interface UploadPFTypes {
+  articleType: string;
+  title: string;
+  content: ContentTypes[];
+  skills: string;
+  tags: string[];
+  teamMembersId: number[];
+}
