9f00437e32f7b7bd5e9e2faa8ccfa4a77f8a76f1
One-HyeWon <5876675@naver.com>
2024-02-12 23:05:10 +0900

    :recycle:[refactor]: mypage 파일 구조 변경

---
+++ src/app/(route)/mypage/[...slug]/page.tsx
@@ -0,0 +1,10 @@
+'use client';
+
+export default function MyPage() {
+  return (
+    <section>
+      <div className="w-[705px] h-[1241px] bg-neutral-900"></div>
+      MyPage
+    </section>
+  );
+}
