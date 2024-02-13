425bcd7a250eeb16db82dcf02c9faa50141dec3d
Minkyu0424 <yng1404@gmail.com>
2024-02-13 23:23:11 +0900

    :sparkles:[feat]: 포트폴리오 게시물 작성 api 연결

--- src/app/recoil/upload.ts
+++ src/app/recoil/upload.ts
@@ -48,24 +48,7 @@ export const uploadRecruitState = atom<UploadRecruitTypes>({
   },
 });
 
-export const userId = atom<number[]>({
-  key: 'userIdFormState',
-  default: [],
-});
-
-export const uploadForm = atom<FormData>({
-  key: 'uploadFormState',
-  default: new FormData(),
-});
-
-export const uploadDataState = atom<UploadPFTypes>({
-  key: 'uploadDataState',
-  default: {
-    articleType: 'PORTFOLIO',
-    title: '',
-    content: [],
-    skills: '',
-    tags: [],
-    teamMembersId: [],
-  },
+export const crewRecruiterState = atom<null>({
+  key: 'crewRecruiterState',
+  default: null,
 });
