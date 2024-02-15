1e8a6b33aa4f71bc3de8eec14f61fea65d10af5f
nayoung3669 <skdud3669@gmail.com>
2024-02-15 15:54:07 +0900

    :ambulance: [hotfix] :디테일 태그 취소 기능

--- src/app/components/main/common/DetailCategories.tsx
+++ src/app/components/main/common/DetailCategories.tsx
@@ -1,19 +1,10 @@
-'use client';
-
 interface DetailCategoriesProps {
   category: string;
-  onRemoveCategory: (arg: string) => void;
 }
 
-const DetailCategories = ({
-  category,
-  onRemoveCategory,
-}: DetailCategoriesProps) => {
+const DetailCategories = ({ category }: DetailCategoriesProps) => {
   return (
-    <div
-      className="flex mr-[20px] w-[105px] max-w-[120px] px-2 h-9 pt-1 bg-gradient-to-r from-[#00E0EE] to-[#517EF3] rounded-3xl justify-center items-center gap-5"
-      onClick={() => onRemoveCategory(category)}
-    >
+    <div className="flex mr-[20px] w-[105px] max-w-[120px] px-2 h-9 pt-1 bg-gradient-to-r from-[#00E0EE] to-[#517EF3] rounded-3xl justify-center items-center gap-5">
       <div># {category}</div>
     </div>
   );
