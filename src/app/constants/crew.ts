a1c56080767cce02a18ae95d57af4eedccfe52de
nayoung3669 <skdud3669@gmail.com>
2024-02-19 18:11:11 +0900

    :sparkles: [feat] : 프로젝트 질문 사항

---
+++ src/app/constants/crew.ts
@@ -0,0 +1,28 @@
+export const HEADER_RECRUITER = {
+  type: '간단 지원서 작성',
+  title: 'weneed을 통한 크루 제안',
+  subtitle: '원하는 크루의 이상향을 말해주세요!',
+} as const;
+
+export const HEADER_APPLICANT = {
+  type: '간단 지원서 작성',
+  title: 'weneed을 통한 크루 제안 작성하기',
+  subtitle: '크루 제안으로 함께하세요!',
+} as const;
+
+export const HEADER_COMPLETE = {
+  title: '간단 크루 지원 모집서 ',
+} as const;
+
+export const RECRUITER_QUESTIONS = {
+  category: '대표적인 카테고리를 선택해주세요.',
+  detailTag: '세부분야를 선택해주세요.',
+  deadline: '모집 기간을 설정해주세요.',
+  description: '하고 있는 프로젝트를 한줄 소개해주세요.',
+  task_need: '어떤 업무를 맡는 크루가 필요하신가요? ',
+  member_count: '필요한 크루의 인원수를 입력해주세요.',
+  phone: '전화번호를 입력해주세요.',
+  crew_questions: '크루를 지원하는 분께 하고 싶은 질문을 작성해주세요.',
+  content: '크루를 하고 싶은 분께 하고 싶은 말을 작성해주세요.',
+  keywords: '이런 분과 함께 하고 싶어요.',
+} as const;
