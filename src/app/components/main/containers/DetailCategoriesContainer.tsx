cefb30d44a6fc2567f55db206dabf79cfd787a4b
nayoung3669 <skdud3669@gmail.com>
2024-02-20 19:19:53 +0900

    :sparkles: [feat] : 라이브러리 변경, conflicts 해결

--- src/app/components/main/containers/DetailCategoriesContainer.tsx
+++ src/app/components/main/containers/DetailCategoriesContainer.tsx
@@ -1,102 +1,56 @@
 'use client';
 
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { Navigation } from 'swiper/modules';
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/scrollbar';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import { SwiperSlide } from 'swiper/react';
 import { bigLeftAngle } from 'ui/IconsPath';
 import Icons from 'components/common/Icons';
 import { INTERESTED_TAG_LIST } from 'constants/portfolio';
 import { selectedCategories } from 'recoil/main';
 import { useRecoilState } from 'recoil';
-import { useEffect, useState } from 'react';
 import DetailCategories from '../common/DetailCategories';
 
 const DetailCategoriesContainer = () => {
   const [selectedCategoriesValue, setSelectedCategories] =
     useRecoilState(selectedCategories);
-  const [slidesPerView, setSlidesPerView] = useState<number>(9.75);
-  const [containerWidth, setContainerWidth] = useState<number | null>(null);
 
-  useEffect(() => {
-    if (
-      selectedCategoriesValue.length === 0 ||
-      selectedCategoriesValue.length > 9
-    ) {
-      setSlidesPerView(9.75);
-    } else {
-      setSlidesPerView(Math.min(selectedCategoriesValue.length));
-    }
-  }, [selectedCategoriesValue]);
-
-  useEffect(() => {
-    const totalWidth = selectedCategoriesValue.length * 125;
-    setContainerWidth(totalWidth);
-  }, [selectedCategoriesValue]);
-
-  const onSelectCategory = (category: string) => {
-    setSelectedCategories((prev) => [...prev, category]);
+  const settings = {
+    initialSlides: 9,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    variableWidth: true,
+    centerPadding: '20px',
+    nextArrow: <CustomNextArrow />,
   };
 
   return (
-    <div className="w-full relative ">
-      <div
-        className="relative flex justify-start items-start w-auto h-9 rounded-3xl gap-[20px]"
-        style={{
-          width:
-            selectedCategoriesValue.length > 9 ||
-            selectedCategoriesValue.length === 0
-              ? '100%'
-              : containerWidth + 'px',
-        }}
+    <div className="w-full relative">
+      <Slider
+        {...settings}
+        className="relative flex justify-center items-center w-full"
       >
-        <Swiper
-          modules={[Navigation]}
-          slidesPerView={slidesPerView}
-          navigation={{
-            nextEl: '.swiper-button-next-category',
-          }}
-          centeredSlides={false}
-          grabCursor={true}
-        >
-          {selectedCategoriesValue.length === 0
-            ? INTERESTED_TAG_LIST.map((category) => {
-                return (
-                  <SwiperSlide key={category}>
-                    <DetailCategories category={category} />
-                  </SwiperSlide>
-                );
-              })
-            : selectedCategoriesValue.map((category) => {
-                return (
-                  <SwiperSlide key={category}>
-                    <DetailCategories category={category} />
-                  </SwiperSlide>
-                );
-              })}
-        </Swiper>
-
-        {(selectedCategoriesValue.length > 9 ||
-          selectedCategoriesValue.length === 0) && (
-          <>
-            <div className="z-20 swiper-button-next-category flex justify-center items-center absolute right-1 top-1.5 w-6 h-6 pl-1 bg-zinc-300 rounded-full cursor-pointer">
-              <Icons name={bigLeftAngle} />
-            </div>
-            <div className="z-10 absolute top-0 right-[-1%] w-32 h-9 bg-gradient-to-r from-transparent to-neutral-950 rounded-xl"></div>
-          </>
-        )}
-      </div>
-      {selectedCategoriesValue.length !== 0 && (
-        <div
-          className=" absolute top-2 right-[5%] font-normal text-sm border-b-1 border-[#FFFFFF] text-[#FFFFFF] cursor-pointer"
-          onClick={() => setSelectedCategories([])}
-        >
-          초기화
-        </div>
-      )}
+        {selectedCategoriesValue.length === 0
+          ? INTERESTED_TAG_LIST.map((category) => (
+              <SwiperSlide key={category}>
+                <DetailCategories category={category} />
+              </SwiperSlide>
+            ))
+          : selectedCategoriesValue.map((category) => (
+              <SwiperSlide key={category}>
+                <DetailCategories category={category} />
+              </SwiperSlide>
+            ))}
+      </Slider>
+      <div className="z-10 absolute top-1 right-[-1%] w-32 h-9 bg-gradient-to-r from-transparent to-blue-950 rounded-xl"></div>
     </div>
   );
 };
 
 export default DetailCategoriesContainer;
+
+const CustomNextArrow = () => (
+  <div className="z-20 swiper-button-next-category flex justify-center items-center absolute right-1 top-1.5 w-6 h-6 pl-1 bg-zinc-300 rounded-full cursor-pointer">
+    <Icons name={bigLeftAngle} />
+  </div>
+);
