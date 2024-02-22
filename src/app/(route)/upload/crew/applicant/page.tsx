95b9fccbf302a69cbbbbdd95d0fbc0c0e6bd212e
nayoung3669 <skdud3669@gmail.com>
2024-02-22 14:30:26 +0900

    :recycle: [hotfix]: 경로 변경

--- src/app/(route)/upload/crew/applicant/page.tsx
+++ src/app/(route)/upload/crew/applicant/page.tsx
@@ -1,5 +1,10 @@
+import CrewHeader from 'components/upload/crew/common/CrewHeader';
+import { HEADER_APPLICANT } from 'constants/crew';
+
 export default async function CrewApplicantPage() {
   return (
-    <section className="flex flex-col items-center justify-center w-full min-h-screen"></section>
+    <section className="flex flex-col items-center justify-center w-full min-h-screen">
+      <CrewHeader header={HEADER_APPLICANT} />
+    </section>
   );
 }
