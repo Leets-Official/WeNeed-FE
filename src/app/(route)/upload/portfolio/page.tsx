286a5da3eefea723e8ccf96f15a3583bcfd1e4a1
Minkyu0424 <yng1404@gmail.com>
2024-02-26 17:50:27 +0900

    :recycle:[refactor]: 게시물 작성 토큰 포함, 공통 요청 코드

--- src/app/(route)/upload/portfolio/page.tsx
+++ src/app/(route)/upload/portfolio/page.tsx
@@ -1,11 +1,20 @@
+'use client';
 import Header from 'components/layout/Header';
 import UploadContainerP from 'components/upload/portfolio/containers/UploadContainerP';
+import { useSearchParams } from 'next/navigation';
 
-export default async function UploadPortfolioPage() {
+export default function UploadPortfolioPage() {
+  const searchParams = useSearchParams();
+  const nickname = searchParams.get('nickname') || '';
+  const userId = searchParams.get('userId') || 0;
   return (
-    <section className="flex flex-col items-center w-full min-h-screen bg-black">
-      {/* <Header isLoggedIn type="main" /> */}
-      <UploadContainerP />
+    <section className="flex flex-col items-center min-h-screen bg-black">
+      <div className=" w-[1280px] mx-auto ">
+        <Header nickname={nickname} userId={Number(userId)} />
+        <div className="w-[90%] mx-auto">
+          <UploadContainerP />
+        </div>
+      </div>
     </section>
   );
 }
--- src/app/api/upload/recruit/route.ts
+++ src/app/api/upload/recruit/route.ts
@@ -1,12 +1,14 @@
 import { NextResponse } from 'next/server';
-import { uploadRecruit } from 'service/uploadRecruit';
+import { uploadRecruit } from 'service/uploadRequest';
+import { getCookie } from 'utils/cookieUtils';
 
 export async function POST(request: Request) {
-  console.log('리크루팅 작성 넥스트 서버에서 받은 요청 데이터', request);
+  const accessToken = getCookie(request, 'accessToken');
+  console.log('리크루팅 작성 넥스트 서버 요청 데이터', request);
   try {
     const res = await request.formData();
     console.log('실제 서버로 요청할 데이터', res);
-    const data = await uploadRecruit(res);
+    const data = await uploadRecruit(res, accessToken);
     return new Response(JSON.stringify(res), {
       headers: {
         'Content-Type': 'multipart/form-data',
