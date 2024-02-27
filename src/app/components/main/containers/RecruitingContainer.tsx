115b85bcfe2ce96a8df55b7937a0aae782b8f3a3
nayoung3669 <skdud3669@gmail.com>
2024-02-27 15:52:42 +0900

    :sparkles: [feat] : 무한 스크롤 (페이징)적용

--- src/app/components/main/containers/RecruitingContainer.tsx
+++ src/app/components/main/containers/RecruitingContainer.tsx
@@ -1,39 +1,50 @@
+'use client';
+
 import Link from 'next/link';
 import RecruitingItem from '../recruiting/RecruitingItem';
 import Profile from 'components/details/common/Profile';
+import useIntersection from 'hooks/main/useIntersection';
 
 interface RecruitingContainerProps {
   data: RecruitListItem[];
-  user: UserProfile;
+  onIntersect: IntersectionObserverCallback;
 }
 
-const RecruitingContainer = ({ data, user }: RecruitingContainerProps) => {
+const RecruitingContainer = ({
+  data,
+  onIntersect,
+}: RecruitingContainerProps) => {
+  const { setTarget } = useIntersection({ onIntersect });
+
   return (
-    <div className="mt-[75px] flex flex-col gap-[50px] w-full">
-      {data.map((article) => {
-        const { nickname, major, grade, createdAt, profile } = article;
-        return (
-          <div key={article.articleId}>
-            <div className="mb-[11px]">
-              <Profile
-                writer={{
-                  major,
-                  grade,
-                  profile: profile,
-                  userId: null,
-                  writerNickname: nickname,
-                }}
-                date={createdAt}
-                size="large"
-              />
+    <>
+      <div className="mt-[75px] flex flex-col gap-[50px] w-full">
+        {data.map((article) => {
+          const { nickname, major, grade, createdAt, profile } = article;
+          return (
+            <div key={article.articleId}>
+              <div className="mb-[11px]">
+                <Profile
+                  writer={{
+                    major,
+                    grade,
+                    profile: profile,
+                    userId: null,
+                    writerNickname: nickname,
+                  }}
+                  date={createdAt}
+                  size="large"
+                />
+              </div>
+              <Link href={`/recruiting/${article.articleId}`}>
+                <RecruitingItem article={article} />
+              </Link>
             </div>
-            <Link href={`/recruiting/${article.articleId}`}>
-              <RecruitingItem article={article} />
-            </Link>
-          </div>
-        );
-      })}
-    </div>
+          );
+        })}
+      </div>
+      <div ref={setTarget}></div>
+    </>
   );
 };
 
