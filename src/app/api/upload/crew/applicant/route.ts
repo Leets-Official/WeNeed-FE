ba4e8a83a71633d31ace34d4b1225d1dc34e708c
nayoung3669 <skdud3669@gmail.com>
2024-03-05 18:58:36 +0900

    :sparkles: [feat] : api 연결 요청 코드

---
+++ src/app/api/upload/crew/applicant/route.ts
@@ -0,0 +1,20 @@
+import { NextResponse } from 'next/server';
+import { postApplicant } from 'service/postRequests';
+import { getCookie } from 'utils/cookieUtils';
+
+export async function POST(request: Request): Promise<NextResponse> {
+  const accessToken = getCookie(request, 'accessToken');
+  const { searchParams } = new URL(request.url);
+  const articleId = searchParams.get('articleId') || '';
+  const res = await request.formData();
+  const data = await postApplicant(articleId, res, accessToken).then(
+    (data) => data,
+  );
+
+  //   return NextResponse.json(JSON.stringify(data), {
+  //     headers: {
+  //       'Content-Type': 'multipart/form-data',
+  //     },
+  //   });
+  return NextResponse.json(data);
+}
