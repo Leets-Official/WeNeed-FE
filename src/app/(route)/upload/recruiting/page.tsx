5fc50042a2190d420e486c4ec889cd11734f2241
Minkyu0424 <yng1404@gmail.com>
2024-03-21 11:58:43 +0900

    :recycle:[refactor]: 작성페이지 리코일 초기화화

--- src/app/(route)/upload/recruiting/page.tsx
+++ src/app/(route)/upload/recruiting/page.tsx
@@ -1,12 +1,20 @@
 'use client';
 import Header from 'components/layout/Header';
 import UploadContainerR from 'components/upload/recruiting/containers/UploadContainerR';
+import useInit from 'hooks/upload/useInit';
 import { useSearchParams } from 'next/navigation';
+import { useEffect } from 'react';
 
 export default function UploadRecruitPage() {
   const searchParams = useSearchParams();
   const nickname = searchParams.get('nickname') || '';
   const userId = searchParams.get('userId') || 0;
+  const { initRC } = useInit();
+
+  useEffect(() => {
+    initRC();
+  }, []);
+
   return (
     <section className="flex flex-col items-center min-h-screen bg-black">
       <div className=" w-[1280px] mx-auto ">
