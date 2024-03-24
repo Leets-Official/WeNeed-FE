15f5c3d5de4ec2020d90769d923d65306c22f8ae
nayoung3669 <skdud3669@gmail.com>
2024-03-24 20:47:34 +0900

    :ambulance: [hotfix] : 검색페이지 userId -1d일 때 추가

--- src/app/(route)/error.tsx
+++ src/app/(route)/error.tsx
@@ -1,7 +1,11 @@
 'use client';
 
+import Button from 'components/common/Button';
+import Icons from 'components/common/Icons';
+import Link from 'next/link';
 import { useRouter } from 'next/navigation';
 import { useEffect } from 'react';
+import { weneed } from 'ui/IconsPath';
 
 export default function GlobalError({
   error,
@@ -19,8 +23,22 @@ export default function GlobalError({
   return (
     <html>
       <body>
-        <h2>문제가 발생했습니다.</h2>
-        <button onClick={() => reset()}>reset</button>
+        <div className="bg-black w-screen h-screen text-white flex flex-col justify-center items-center gap-10">
+          <h2 className="text-3xl font-bold">문제가 발생했습니다.</h2>
+          <Button
+            type="upload_recruiter"
+            className="hover:bg-white"
+            isDisabled={false}
+            buttonText="새로고침"
+            onClickHandler={() => reset()}
+          />
+          <Link
+            href={'/'}
+            className="flex flex-col justify-center items-center"
+          >
+            홈으로 <Icons name={weneed} />
+          </Link>
+        </div>
       </body>
     </html>
   );
--- src/app/(route)/search/[...slug]/layout.tsx
+++ src/app/(route)/search/[...slug]/layout.tsx
@@ -6,6 +6,7 @@ export default function SearchPageLayout({
   return (
     <section className="bg-black">
       <div className="max-w-[1280px] min-w-[1100px] mx-auto ">{children}</div>
+      <div id="needLoginPortal" />
     </section>
   );
 }
