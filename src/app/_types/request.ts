b374f8e2ccc66b68aecd1d81d5c212f52e956762
One-HyeWon <5876675@naver.com>
2024-03-24 18:55:27 +0900

    :sparkle:[feat]: 마이페이지 수정 UI

--- src/app/_types/request.ts
+++ src/app/_types/request.ts
@@ -6,3 +6,16 @@ interface RequestMainRecruitParams {
 interface RequestMainPortfolioParams extends RequestMainRecruitParams {
   sort: string[];
 }
+
+interface MyPageMyInfoPatch {
+  profileImage: string;
+  request: {
+    nickname: string;
+    major: string;
+    userGrade: number;
+    doubleMajor: string;
+    interestFiled: string;
+    links: string[];
+  };
+  selfIntro: string;
+}
