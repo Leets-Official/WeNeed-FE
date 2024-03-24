e6326454e9969d74864c119b09a282ef4d82ac25
Minkyu0424 <yng1404@gmail.com>
2024-03-24 16:03:38 +0900

    :ambulance:[hotfix]: 불필요 log 삭제, switch default 비우기

--- src/app/api/update/recruiting/route.ts
+++ src/app/api/update/recruiting/route.ts
@@ -3,13 +3,11 @@ import { updateRecruit } from 'service/updateRequest';
 import { getCookie } from 'utils/cookieUtils';
 
 export async function PATCH(request: Request) {
-  console.log('포폴수정 요청 데이터', request);
   const { searchParams } = new URL(request.url);
   const id = searchParams.get('articleId') || '';
   const accessToken = getCookie(request, 'accessToken');
   try {
     const res = await request.formData();
-    console.log('실제 서버로 요청할 데이터', res);
     const data = await updateRecruit(res, accessToken, id);
     return new Response(JSON.stringify(res), {
       headers: {
@@ -17,7 +15,6 @@ export async function PATCH(request: Request) {
       },
     });
   } catch (error) {
-    console.error('넥스트 서버에서 PATCH 요청 처리 중 오류 발생:', error);
     return NextResponse.json({ error: 'Internal Server Error' });
   }
 }
