c49b25555edf32688bd57f2a29140e149da84c0c
One-HyeWon <5876675@naver.com>
2024-03-24 21:18:14 +0900

    :recycle:[refactor]: 주석 제거

--- src/app/components/mypage/container/PostContainer.tsx
+++ src/app/components/mypage/container/PostContainer.tsx
@@ -36,33 +36,3 @@ const PostContainer = ({
 };
 
 export default PostContainer;
-
-// const myOutputMockList = [
-//   {
-//     articleId: 93,
-//     thumbnail:
-//       'https://s3.ap-northeast-2.amazonaws.com/weneedbucket/files/d4d73ffd-ff47-4d23-b1bf-153d5f5d00a9.png',
-//     teamProfiles: ['1', '2', '3'],
-//     title: '피드 제목입니다.',
-//     viewCount: 1,
-//     heartCount: 0,
-//   },
-//   {
-//     articleId: 92,
-//     thumbnail:
-//       'https://s3.ap-northeast-2.amazonaws.com/weneedbucket/files/f768beab-0306-45b9-8a28-f97a3f165c16.jpg',
-//     teamProfiles: [],
-//     title: '피드 제목',
-//     viewCount: 2,
-//     heartCount: 0,
-//   },
-//   {
-//     articleId: 91,
-//     thumbnail:
-//       'https://s3.ap-northeast-2.amazonaws.com/weneedbucket/files/3e654873-b99f-4df7-b1d9-703dc3bcaf7c.png',
-//     teamProfiles: [],
-//     title: '피드 제목',
-//     viewCount: 2,
-//     heartCount: 0,
-//   },
-// ];
