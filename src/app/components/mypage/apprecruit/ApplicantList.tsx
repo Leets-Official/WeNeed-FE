527c365c08a01e1d3736072ef03d095e655719e1
One-HyeWon <5876675@naver.com>
2024-04-13 16:04:21 +0900

    :ambulance:[refactor]: 지원서 공유하기 수정

--- src/app/components/mypage/apprecruit/ApplicantList.tsx
+++ src/app/components/mypage/apprecruit/ApplicantList.tsx
@@ -36,13 +36,19 @@ const ApplicantList = ({ type }: ApplicantListProps) => {
         ? acceptedList
         : refusedList;
 
+  const copyURL = async (applicantId: number) => {
+    const currentUrl = `www.weneed.site/crew/application/${applicantId}`;
+    await navigator.clipboard.writeText(currentUrl);
+    alert('링크가 복사되었습니다.');
+  };
+
   return (
     <div className="w-full flex gap-8 flex-col items-center scrollbar-hide">
       <div className="w-full scrollbar-hide">
         {applicationList.map((applicant, index) => (
           <div
             key={applicant.applicationItem.applicationId}
-            className={`flex relative border-black items-center w-full h-[140px] border ${
+            className={`flex relative border-black items-center w-full justify-between h-[140px] border ${
               applicant.selected ? ' bg-black' : 'bg-white'
             } rounded-[15px] mb-4`}
           >
@@ -61,63 +67,72 @@ const ApplicantList = ({ type }: ApplicantListProps) => {
               )}
             </div>
             <Link
-              className="flex relative items-center"
+              className="flex relative items-center w-[80%]"
               href={`/crew/application/${applicant.applicationItem.applicationId}`}
             >
-              <div className="rounded-full overflow-hidden h-[57px] w-[57px] mx-10">
-                {applicant.applicationItem.user.profile ? (
-                  <Image
-                    src={applicant.applicationItem.user.profile}
-                    alt="profile"
-                    width={57}
-                    height={57}
-                    style={{
-                      objectFit: 'cover',
-                      objectPosition: 'center center',
-                      width: '100%',
-                      height: '100%',
-                    }}
-                  />
-                ) : (
-                  <GradientProfileMD />
-                )}
-              </div>
-              <div className="h-full ml-6 flex flex-col justify-center gap-3">
-                <div
-                  className={`${
-                    applicant.selected ? 'text-white' : 'text-black'
-                  } text-3xl font-bold`}
-                >
-                  {applicant.applicationItem.user.nickname} [
-                  {applicant.applicationItem.user.major}] 지원서
+              <div className="flex relative items-center w-full">
+                <div className="rounded-full overflow-hidden h-[57px] w-[57px] mx-10">
+                  {applicant.applicationItem.user.profile ? (
+                    <Image
+                      src={applicant.applicationItem.user.profile}
+                      alt="profile"
+                      width={57}
+                      height={57}
+                      style={{
+                        objectFit: 'cover',
+                        objectPosition: 'center center',
+                        width: '100%',
+                        height: '100%',
+                      }}
+                    />
+                  ) : (
+                    <GradientProfileMD />
+                  )}
                 </div>
-                <div
-                  className={`${
-                    applicant.selected ? 'text-white' : 'text-black'
-                  } text-sm font-normal`}
-                >
-                  {applicant.applicationItem.user.major} |{' '}
-                  {applicant.applicationItem.user.grade}학년 |{' '}
-                  {
-                    new Date(applicant.applicationItem.appliedAt)
-                      .toISOString()
-                      .split('T')[0]
-                  }
+                <div className="h-full ml-6 flex flex-col justify-center gap-3">
+                  <div
+                    className={`${
+                      applicant.selected ? 'text-white' : 'text-black'
+                    } text-3xl font-bold`}
+                  >
+                    {applicant.applicationItem.user.nickname} [
+                    {applicant.applicationItem.user.major}] 지원서
+                  </div>
+                  <div
+                    className={`${
+                      applicant.selected ? 'text-white' : 'text-black'
+                    } text-sm font-normal`}
+                  >
+                    {applicant.applicationItem.user.major} |{' '}
+                    {applicant.applicationItem.user.grade}학년 |{' '}
+                    {
+                      new Date(applicant.applicationItem.appliedAt)
+                        .toISOString()
+                        .split('T')[0]
+                    }
+                  </div>
                 </div>
               </div>
             </Link>
 
             <div
-              className={`absolute right-4 flex items-end pb-5 gap-3 ${
+              className={`relative w-[10%] right-0 bottom-0 flex items-end pb-5 gap-3 ${
                 applicant.selected ? 'text-white' : 'text-black'
-              } text-sm h-full`}
+              } h-full`}
             >
-              <Icons
-                name={
-                  applicant.selected ? applicantShareWhite : applicantShareBlack
-                }
-              />
-              공유하기
+              <div
+                className={`relative flex items-center gap-2 cursor-pointer`}
+                onClick={() => copyURL(applicant.applicationItem.applicationId)}
+              >
+                <Icons
+                  name={
+                    applicant.selected
+                      ? applicantShareWhite
+                      : applicantShareBlack
+                  }
+                />
+                <span className="pt-1">공유하기</span>
+              </div>
             </div>
           </div>
         ))}
